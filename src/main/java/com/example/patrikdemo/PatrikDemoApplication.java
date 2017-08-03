411154a6cf16ccd270b045bb3f0851639a4d45bf
Patrik Nagel <pnagel@pivotal.io>
2017-08-03 14:54:44 +0200

    adding demo app

---
+++ src/main/java/com/example/patrikdemo/PatrikDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.patrikdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrikDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrikDemoApplication.class, args);
+	}
+}
